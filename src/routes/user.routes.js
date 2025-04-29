12146ed98a3d2dbe65a4ab6481fb52538870f9e3
shivkumar991832 <shivkumar991832@gmail.com>
2025-04-30 00:28:54 +0530

    adding new route for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refresAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refresAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from './../middlewares/auth.middleware.js';
+
 
 const router = Router()
 
@@ -31,5 +32,13 @@ router.route("/logout").post(verifyJWT, logoutUser)
 // here not required (verifyJWT)
 // making end point of Refresh Token
 router.route("/refresh-token").post(refresAccessToken)
+router.route("/change-password").post( verifyJWT, changeCurrentPassword )
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+// in case of params -- the rule is --
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 export default router
  
